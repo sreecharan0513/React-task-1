814185104900984ebc2acdcf84c9a9e00e9d7cc4
sreecharan0513 <139733279+sreecharan0513@users.noreply.github.com>
2023-10-14 22:33:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState("");
+  const wordCount = state.split(/\s+/).filter(Boolean).length;
+  const handleInput = (e) => {
+    setState(e.target.value);
+  };
+  return (
+    <div className="App">
+      <h4>Responsive Paragraph Word Counter</h4>
+      <textarea
+        placeholder="Text Input"
+        onChange={handleInput}
+        value={state}
+      ></textarea>
+      <p className="wc">
+        Word Count: <span className="num">{wordCount}</span>{" "}
+      </p>
+      <p>Sree Charan Yadagiri</p>
+    </div>
+  );
+}
+
+export default App;
